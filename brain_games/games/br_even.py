54adc0126f4e6823ba56e3b2f5125967ed3f08e0
renzoshi <grandes84@gmail.com>
2023-04-13 21:21:05 +0300

    add brain_games

--- brain_games/games/br_even.py
+++ brain_games/games/br_even.py
@@ -2,13 +2,10 @@ from random import randint
 
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-def is_even(rand_num):
-    return rand_num % 2 == 0
-
 def pass_game():
     rand_num = randint(1, 100)
 
-    if is_even(rand_num):
+    if rand_num % 2 == 0:
         right_answer = 'yes'
     else:
         right_answer = 'no'
