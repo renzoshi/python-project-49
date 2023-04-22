46e5f01956ae110ad02f1bc9e318254a16edfe29
renzoshi <grandes84@gmail.com>
2023-04-22 19:28:06 +0300

    brain_games

--- brain_games/g_logic.py
+++ brain_games/g_logic.py
@@ -4,21 +4,20 @@ import prompt
 ROUND = 3
 
 
-def launch_game(open):
+def launch_game(games):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name?')
     print('Hello, ' + name + '!')
     print(open.RULES)
     for _ in range(ROUND):
-        question, right_answer = open.get_start_game()
+        question, right_answer = games.get_start_game()
         print('Question: ' + str(question))
         answer = prompt.string('Your answer: ')
-        if answer == right_answer or answer == right_answer.capitalize():
-            print('Correct!')
-        else:
+        if answer.lower() != right_answer:
             print(f"'{answer}' is wrong answer ;(. "
                   f"Correct answer was '{right_answer}'."
                   f"\nLet's try again, {name}!")
             return
+    print('Correct!')
 
     print('Congratulations, ' + name + '!')
